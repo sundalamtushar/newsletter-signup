b0424d4c212b8a8f5b0fd9a22666382500ec2a97
sundalamtushar <sundalamtushar@gmail.com>
2023-02-09 21:03:40 +0530

    first commit

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ const https = require("https");
 const port = 2000;
 const app = express();
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUrvmGFyKFjRK6rheh7ib_yBu0Cn5Nz4s",
+  authDomain: "newsletter--signup-817aa.firebaseapp.com",
+  projectId: "newsletter--signup-817aa",
+  storageBucket: "newsletter--signup-817aa.appspot.com",
+  messagingSenderId: "1011708616575",
+  appId: "1:1011708616575:web:fa8f6be5d9e300a475444a",
+  measurementId: "G-LNRWH5ZZDW"
+};
+
+// Initialize Firebase
+app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
